36364498c06d69085919255d1953106b50fd1f70
Xtrau505 <xtrau505true@gmail.com>
2024-12-12 21:39:43 -0600

    Update Shader Fallbacks

--- src/main/java/com/zephyros1938/engine/ShaderUtils.java
+++ src/main/java/com/zephyros1938/engine/ShaderUtils.java
@@ -8,42 +8,50 @@ public class ShaderUtils {
     private static final String FALLBACK_SHADER_VERT = "#version 330\nlayout (location=0) in vec3 position;layout (location=1) in vec3 color;out vec3 outColor;uniform mat4 projectionMatrix;uniform mat4 modelMatrix;void main(){gl_Position = projectionMatrix * modelMatrix * vec4(position, 1.0);outColor = color;}";
     private static final String FALLBACK_SHADER_FRAG = "#version 330\nin vec3 outColor;out vec4 fragColor;void main(){fragColor = vec4(outColor,1.0);}";
 
-    private static final Map<String, Integer> SHADER_FILE_MAP = new HashMap<>();
-    private static final Map<Integer, String> SHADER_FILE_MAP_FALLBACKS = new HashMap<>();
+    private static final Map<String, Integer> SHADER_MAP_KEYS = new HashMap<>();
+    private static final Map<Integer, String> SHADER_MAP_FALLBACKS = new HashMap<>();
+
+    public static final Integer VERTEX_ID = 0;
+    public static final Integer FRAGMENT_ID = 1;
+    public static final Integer GEOMETRY_ID = 2;
+    public static final Integer TESSELLATION_CONTROL_ID = 3;
+    public static final Integer TESSELLATION_EVALUATION_ID = 4;
+    public static final Integer COMPUTE_ID = 5;
 
     static {
-        // Static block to initialize the map with the extensions and corresponding
-        // shader types
-        SHADER_FILE_MAP.put(".vert", 0);
-        SHADER_FILE_MAP.put(".vr", 0);
-        SHADER_FILE_MAP.put(".vsh", 0);
-        SHADER_FILE_MAP.put(".vs", 0);
-        SHADER_FILE_MAP.put(".frag", 1);
-        SHADER_FILE_MAP.put(".fs", 1);
-        SHADER_FILE_MAP.put(".fsh", 1);
-        SHADER_FILE_MAP.put(".geom", 2);
-        SHADER_FILE_MAP.put(".gs", 2);
-        SHADER_FILE_MAP.put(".gsh", 2);
-        SHADER_FILE_MAP.put(".tcs", 3);
-        SHADER_FILE_MAP.put(".tessc", 3);
-        SHADER_FILE_MAP.put(".tes", 4);
-        SHADER_FILE_MAP.put(".tesh", 4);
-        SHADER_FILE_MAP.put(".comp", 5);
-        SHADER_FILE_MAP.put(".cs", 5);
+        // Initialize the map with the extensions and corresponding shader IDs
+        SHADER_MAP_KEYS.put(".vert", VERTEX_ID);
+        SHADER_MAP_KEYS.put(".vr", VERTEX_ID);
+        SHADER_MAP_KEYS.put(".vsh", VERTEX_ID);
+        SHADER_MAP_KEYS.put(".vs", VERTEX_ID);
+        SHADER_MAP_KEYS.put(".frag", FRAGMENT_ID);
+        SHADER_MAP_KEYS.put(".fs", FRAGMENT_ID);
+        SHADER_MAP_KEYS.put(".fsh", FRAGMENT_ID);
+        SHADER_MAP_KEYS.put(".geom", GEOMETRY_ID);
+        SHADER_MAP_KEYS.put(".gs", GEOMETRY_ID);
+        SHADER_MAP_KEYS.put(".gsh", GEOMETRY_ID);
+        SHADER_MAP_KEYS.put(".tcs", TESSELLATION_CONTROL_ID);
+        SHADER_MAP_KEYS.put(".tessc", TESSELLATION_CONTROL_ID);
+        SHADER_MAP_KEYS.put(".tes", TESSELLATION_EVALUATION_ID);
+        SHADER_MAP_KEYS.put(".tesh", TESSELLATION_EVALUATION_ID);
+        SHADER_MAP_KEYS.put(".comp", COMPUTE_ID);
+        SHADER_MAP_KEYS.put(".cs", COMPUTE_ID);
     }
 
     static {
-        SHADER_FILE_MAP_FALLBACKS.put(0, FALLBACK_SHADER_VERT);
-        SHADER_FILE_MAP_FALLBACKS.put(1, FALLBACK_SHADER_FRAG);
-        SHADER_FILE_MAP_FALLBACKS.put(2, "Shader Fallback Not Found");
-        SHADER_FILE_MAP_FALLBACKS.put(3, "Shader Fallback Not Found");
-        SHADER_FILE_MAP_FALLBACKS.put(4, "Shader Fallback Not Found");
+        // Initialize the map with the shader IDs and corresponding fallbacks
+        SHADER_MAP_FALLBACKS.put(VERTEX_ID, FALLBACK_SHADER_VERT);
+        SHADER_MAP_FALLBACKS.put(FRAGMENT_ID, FALLBACK_SHADER_FRAG);
+        SHADER_MAP_FALLBACKS.put(GEOMETRY_ID, "Shader Fallback Not Found");
+        SHADER_MAP_FALLBACKS.put(TESSELLATION_CONTROL_ID, "Shader Fallback Not Found");
+        SHADER_MAP_FALLBACKS.put(TESSELLATION_EVALUATION_ID, "Shader Fallback Not Found");
+        SHADER_MAP_FALLBACKS.put(COMPUTE_ID, "Shader Fallback Not Found");
     }
 
     public static String getShaderFallback(String file_path) {
-        for (String extension : SHADER_FILE_MAP.keySet()) {
+        for (String extension : SHADER_MAP_KEYS.keySet()) {
             if (file_path.endsWith(extension)) {
-                return SHADER_FILE_MAP_FALLBACKS.get(SHADER_FILE_MAP.get(extension));
+                return SHADER_MAP_FALLBACKS.get(SHADER_MAP_KEYS.get(extension));
             }
         }
         return null;
